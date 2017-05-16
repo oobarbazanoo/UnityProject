939e96dc6eff261ab2894b88adc281423b754e1e
oobarbazanoo <yayarokya@mail.ru>
2017-05-16 17:02:06 +0300

    oh! yeah!!!
    first task is done!

---
+++ Assets/Content/Scripts/CameraConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraConfig : MonoBehaviour
+{
+    public RabbitController objToFollow;
+
+	// Use this for initialization
+	void Start()
+    {
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        Transform rabit_transform = objToFollow.transform;
+        Transform camera_transform = this.transform;
+
+        Vector3 rabit_position = rabit_transform.position;
+        Vector3 camera_position = camera_transform.position;
+
+        camera_position.x = rabit_position.x;
+        camera_position.y = rabit_position.y;
+
+        camera_transform.position = camera_position;
+    }
+}
