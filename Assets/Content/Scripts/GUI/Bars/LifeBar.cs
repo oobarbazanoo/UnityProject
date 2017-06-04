982bf592cefa80f4fe9c4ebff215112a640b1577
Yaroslav <yayarokya@gmail.com>
2017-06-04 20:19:33 +0300

    life bar tweaked
    life bar tweaked

---
+++ Assets/Content/Scripts/GUI/Bars/LifeBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeBar : MonoBehaviour
+{
+     void Awake()
+    {
+        LevelController.SetLifeBar(this.gameObject);
+    }
+
+
+}
--- Assets/Content/Scripts/TweakStuff/LevelController.cs
+++ Assets/Content/Scripts/TweakStuff/LevelController.cs
@@ -1,17 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
     Vector3 startingPosition;
     private static GameObject rabbitObj;
+    private static GameObject lifeBarObj;
+
+    private int lives;
 
     public void setStartPosition(Vector3 pos)
-    {
-        this.startingPosition = pos;
-    }
+    {this.startingPosition = pos;}
 
     public static void SetRabbit(GameObject rabbit)
     { rabbitObj = rabbit; }
+
+    public static void SetLifeBar(GameObject lifeBar)
+    { lifeBarObj = lifeBar; }
+
+    public static GameObject GetLifeBar()
+    {
+        if (lifeBarObj == null)
+        { throw new UnityException("lifeBarObj is not set yet!"); }
+        return lifeBarObj;
+    }
 
     public void onRabitDeath(RabbitController rabit)
     {
@@ -19,6 +33,26 @@ public class LevelController : MonoBehaviour
         rabit.transform.position = this.startingPosition;
         RabbitStats rabbitStats = rabit.gameObject.GetComponent<RabbitStats>();
         rabbitStats.isDead = false;
+
+        if(lives == 0)
+        { SceneManager.LoadScene("chooseLevel"); }
+        else
+        { decrementLives(); }
+    }
+
+    private void decrementLives()
+    {
+        decrementLivesInLifeBar();
+        lives--;
+    }
+
+    private void decrementLivesInLifeBar()
+    {
+        GameObject lifeBar = GetLifeBar();
+
+        GameObject heartToChange = lifeBar.transform.Find(lives + "").gameObject;
+
+        heartToChange.GetComponent<UI2DSprite>().sprite2D = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Content/UI/level/notLife.png", typeof(Sprite));
     }
 
     public static GameObject getRabbit()
@@ -28,6 +62,7 @@ public class LevelController : MonoBehaviour
     void Awake()
     {
         current = this;
+        lives = 3;
     }
 
     // Use this for initialization
